edb23b20b325e3202cf43130ed05ec28406a704b
Alex Wond <kristalga@gmail.com>
2023-02-28 20:57:50 +0200

    Add locators to F_1SpelioneSpejimai26 class

--- src/test/java/lt/aleksandras/f_1/pom/pages/Locator.java
+++ src/test/java/lt/aleksandras/f_1/pom/pages/Locator.java
@@ -6,7 +6,7 @@ public class Locator {
     public static class F_1PageMain {
 
         public static By buttonPrisijungti = By.xpath("//a[@data-target='#login']");
-        public static By buttonCloseAdvertisment = By.xpath("//img[@id]");
+        public static By buttonCloseAdvertisment = By.xpath("(//img[@id])[1]");
         public static By buttonSupratauCookies = By.xpath("//a[@aria-label='dismiss cookie message']");
         public static By buttonRegistruokites = By.xpath("//a[@id='register-modal-trigger']");
         public static By registrationForm = By.xpath("(//div[@class='modal-content'])[2]");
@@ -22,7 +22,7 @@ public class Locator {
         public static By passwordInput = By.xpath("//input[@id='register-password']");
         public static By passwordConfirmInput = By.xpath("//input[@id='register-confirm-password']");
         public static By registerButton = By.xpath("//button[@id='register-button']");
-        public static By registerError = By.xpath("//div[@id='register-error']");
+        public static By registerMessage = By.xpath("//div[@id='register-error']");
         public static By loginUserName = By.xpath("//input[@id='login-username']");
         public static By loginPassword = By.xpath("//input[@id='login-password']");
         public static By loginPrisijungit = By.xpath("//button[@id='login-button']");
@@ -36,6 +36,12 @@ public class Locator {
         public static By buttonPatvirtinti = By.xpath("//button[@type='submit']");
         public static By guessConfirmMessage = By.xpath("//div[@class='alert alert-success']");
         public static By selectedValuePP = By.xpath("(//select[@name='pp']//option[@selected])[2]");
+        public static By bannerDraugasLt = By.xpath("//iframe[@name='draugas']");
+        public static By bannerDraugas = By.xpath("//a[@style='position: absolute; top: 0px; left: 0px;" +
+                " bottom: 0px; right: 0px; z-index: 9999999999; cursor: pointer;']");
+        public static By buttonCloseAdvertisment = By.xpath("(//img[@id])[1]");
+        public static By buttonSupratauCookies = By.xpath("//a[@aria-label='dismiss cookie message']");
+
 
         public static By placePosition(int place) {
             return By.xpath("//select[@name='" + place + "']");
